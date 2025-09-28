da7a1dde7ba3731e499caf9183a27dc8c3eb33ab
ishitadey955 <ishitadey955@gmail.com>
2025-09-29 00:32:06 +0530

    Update axiosInstance.js

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
+const API_BASE_URL = https://luminae-c85h.onrender.com
   import.meta.env.VITE_API_URL || "http://localhost:5000"; // fallback for local dev
 
 const axiosInstance = axios.create({
